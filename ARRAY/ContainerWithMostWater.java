cee0f30097583b42bae29f543b38a98dad208609
Gaurav <gs177137@gmail.com>
2026-03-07 22:40:34 +0530

    2 questions added

---
+++ ARRAY/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/container-with-most-water
+
+
+public class ContainerWithMostWater {
+    static { for(int i = 0;i <100;i++) { maxArea(new int[] { 0, 0 }); } }
+    public static int maxArea(int[] height) {
+        int n=height.length;
+        int maxArea=0;
+        for (int i=0,j=n-1;i<j;) {
+            int minH=Math.min(height[i], height[j]);
+            int area=(j-i)*minH;
+            maxArea=Math.max(area,maxArea);
+            while(i<j && height[i]<=minH) i++;
+            while(i<j && height[j]<=minH) j--;
+        }
+        return maxArea;
+    }
+}
